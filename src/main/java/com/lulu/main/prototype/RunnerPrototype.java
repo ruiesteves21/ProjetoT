fa704633eca1aeee3179d2292bd382b1851792d9
ruesteves <ruipedro21esteves@gmail.com>
2023-05-19 13:04:50 +0100

    Update RunnerPrototype.java

--- src/main/java/com/lulu/main/prototype/RunnerPrototype.java
+++ src/main/java/com/lulu/main/prototype/RunnerPrototype.java
@@ -6,21 +6,11 @@ import java.io.IOException;
 
 public class RunnerPrototype {
     public static void main(String[] args) throws IOException {
-        /*String dslYamlPath = "DslPrototype.yml";
+        String dslYamlPath = "DslPrototype.yml";
         String toolYamlPath = "ToolPrototype.yml";
         ClassLoader classLoader = RunnerPrototype.class.getClassLoader();
         String pathToYaml = classLoader.getResource(dslYamlPath).getPath();
         String pathToAuxYaml = classLoader.getResource(toolYamlPath).getPath();
-         */
-
-
-        String dslYamlURL = "https://raw.githubusercontent.com/ruiesteves21/ProjetoT/main/DslPrototype.yml";
-        String toolYamlURL = "https://raw.githubusercontent.com/ruiesteves21/ProjetoT/main/ToolPrototype.yml";
-
-        // Use the URLs in your code
-        String pathToYaml = dslYamlURL;
-        String pathToAuxYaml = toolYamlURL;
-
 
         new DslRunner(pathToYaml, pathToAuxYaml);
     }
